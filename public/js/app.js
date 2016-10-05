166efedd4595cbf1730c130f9762a3c76d86153b
Jiheh Ritterling <jritterling@gmail.com>
2016-10-05 11:00:25 -0400

    minor changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,20 @@
 var map = initializeMap();
 
 $(".pull-right").click(function() {
+	// Add to itinerary
 	var type = $(this).parent().find("select").data("type");
+	var name = ($(this).parent().find("select option:selected").val());
+
+	// if (type === 'hotel') {
+		console.log($('ul:contains("hotel-group")').find('div'));
+	// } else if (type === 'restaurant') {
+
+	// } else if (type === 'activity') {
+
+	// }
+
+
+	// Add to map
 	var location = $(this).parent().find("select option:selected").data("location").split(",");
 
 	map.setCenter({
